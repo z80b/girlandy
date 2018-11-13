b4212cfac99ab712cbe13a1e2c51abb622beb7eb
z80b <akozlov78@gmail.com>
2018-11-13 14:11:31 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp         = require('gulp'),
+    stylus       = require('gulp-stylus'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS     = require('gulp-clean-css'),
+    rename       = require('gulp-rename'),
+    axis         = require('axis'),
+    __basedir    = __dirname;
+
+gulp.task('styles', () => {
+    return gulp
+        .src('./src/css/index.styl')
+        .pipe(stylus({
+            'include css': true,
+            'compress': false,
+            'use': [axis()],
+            'rawDefine': { 'inline-image': stylus.stylus.url({
+                paths: ['./src/css/imgs']
+            }) }
+        }))
+        .pipe(autoprefixer(['> 0%']))
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest('.')); 
+});
+
+
+gulp.task('default', ['styles'], function() {
+    gulp.watch('src/**/*.*', ['styles']);
+});
